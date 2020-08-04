1e5ea98245ee489e8f21be036bac38f030fb14a5
Author <43812455+Userrnam@users.noreply.github.com>
2020-08-05 01:03:52 +0500

    .

--- AGE/include/Graphics/ShaderBuilder.hpp
+++ AGE/include/Graphics/ShaderBuilder.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "Components/IGraphicsComponent.hpp"
+#include "Shader.hpp"
 
 
 namespace age {
@@ -16,6 +17,9 @@ public:
     void generateVertexShaderSource(const std::vector<IGraphicsComponent*>& components);
     void generateFragmentShaderSource(const std::vector<IGraphicsComponent*>& components);
     void saveShader(const std::string& path);
+
+    Shader compileVertexShader(const std::vector<IGraphicsComponent*>& components);
+    Shader compileFragmentShader(const std::vector<IGraphicsComponent*>& components);
 
     void clear() {
         m_stream.str(std::string());
